ccf668baac1d41b58ff69826ba29908f50dfe964
shadi <shadisharifv@gmail.com>
2020-12-16 17:01:36 +0100

    something

---
+++ server/demo/src/main/java/com/example/demo/Controller/TaskDataController.java
@@ -0,0 +1,14 @@
+package com.example.demo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+public class TaskDataController {
+
+
+
+}
