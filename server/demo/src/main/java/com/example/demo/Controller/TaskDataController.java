0eeb7a8ed597808e6682fae47fdd1b24bbefbf43
shadi <shadisharifv@gmail.com>
2020-12-16 17:15:30 +0100

    Controller updated

--- server/demo/src/main/java/com/example/demo/Controller/TaskDataController.java
+++ server/demo/src/main/java/com/example/demo/Controller/TaskDataController.java
@@ -3,6 +3,7 @@ package com.example.demo.Controller;
 import com.example.demo.Model.Task;
 import com.example.demo.Service.TaskDataService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,14 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:8000")
 @RestController
 public class TaskDataController {
     @Autowired
     private TaskDataService tservice;
 
-    //public TaskDataController() {
-        //tservice = new TaskDataService();
-    //}
 
     @GetMapping("/getTasks")
     public List<Task> getTypes(){
